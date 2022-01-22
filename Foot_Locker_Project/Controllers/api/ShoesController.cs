4174d47cee1b5c9c8fbc347ca08d2188a0d540bf
Lior-Yosef <lioryosef06@gmail.com>
2022-01-22 20:50:45 +0200

    add new actions

---
+++ Foot_Locker_Project/Controllers/api/ShoesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Foot_Locker_Project.Models;
+
+namespace Foot_Locker_Project.Controllers.api
+{
+    public class ShoesController : Controller
+    {
+        static string StringConnection = "Data Source=LAPTOP-OT5IVM7S;Initial Catalog=SportStoreDB;Integrated Security=True;Pooling=False";
+        SportStoreDB db = new SportStoreDB(StringConnection);  
+        // GET: Shoes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAllShoes()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Foot_Locker_Project/Controllers/api/SportStoreDB.cs
@@ -0,0 +1,12 @@
+﻿namespace Foot_Locker_Project.Controllers.api
+{
+    internal class SportStoreDB
+    {
+        private string stringConnection;
+
+        public SportStoreDB(string stringConnection)
+        {
+            this.stringConnection = stringConnection;
+        }
+    }
+}
